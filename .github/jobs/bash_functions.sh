2246c18dca2c50cad90c41b240462fe1ffb5fa5a
John Halley Gotway <johnhg@ucar.edu>
2025-07-10 15:28:08 -0600

    Working on the pull and scan workflow

--- .github/jobs/bash_functions.sh
+++ .github/jobs/bash_functions.sh
@@ -29,13 +29,13 @@ function time_command {
 # utilty function to scan a Docker image for vulnerabilities
 function cve_scan_image {
   echo "Scanning image $1"
-  LOG_FILE="${GITHUB_WORKSPACE}/logs/CVE_Scan_`echo $1 | sed 's%/%_%g'`.log"
-  time_command "grype $1 > ${LOG_FILE} 2>&1"
-  N_CRITICAL=`grep "Critical" ${LOG_FILE} | wc -l`
-  if [ $N_CRITICAL > 0 ]; then
-    echo "WARNING: Found ${N_CRITICAL} CVEs for image $1 in ${LOG_FILE}:"
+  CMD_LOGFILE="${GITHUB_WORKSPACE}/logs/CVE_Scan_`echo $1 | sed 's%/%_%g'`.txt"
+  time_command grype $1
+  N_CRITICAL=`grep "Critical" ${CMD_LOGFILE} | wc -l`
+  if [ $N_CRITICAL -gt 0 ]; then
+    echo "WARNING: Found ${N_CRITICAL} CVEs for image $1 in ${CMD_LOGFILE}"
     echo
-    egrep "SEVERITY|Critical" ${LOG_FILE}
+    egrep "SEVERITY|Critical" ${CMD_LOGFILE}
     echo
   fi
 }
