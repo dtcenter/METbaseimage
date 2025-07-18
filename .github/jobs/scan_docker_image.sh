ba050f532ad57d53557e19007513a37a01a472e7
John Halley Gotway <johnhg@ucar.edu>
2025-07-18 13:03:51 -0600

    Fix issues in scan_docker_image.sh

--- .github/jobs/scan_docker_image.sh
+++ .github/jobs/scan_docker_image.sh
@@ -3,12 +3,15 @@
 source ${GITHUB_WORKSPACE}/.github/jobs/bash_functions.sh
 
 # Scan the images
-ret1 = cve_scan_image ${DOCKERHUB_BASE_REPO}:${GITHUB_NAME}
-ret2 = cve_scan_image ${DOCKERHUB_UNIT_TEST_REPO}:${GITHUB_NAME}
-ret3 = cve_scan_image ${DOCKERHUB_METVIEWER_REPO}:${GITHUB_NAME}
+cve_scan_image ${DOCKERHUB_BASE_REPO}:${GITHUB_NAME}
+retval1=$?
+cve_scan_image ${DOCKERHUB_UNIT_TEST_REPO}:${GITHUB_NAME}
+retval2=$?
+cve_scan_image ${DOCKERHUB_METVIEWER_REPO}:${GITHUB_NAME}
+retval3=$?
 
 # Check for bad return status
-if [[ $ret1 -ne 0 || $ret2 -ne 0 || $ret3 -ne0 ]]; then
+if [[ $retval1 -ne 0 || $retval2 -ne 0 || $retval3 -ne 0 ]]; then
   echo "ERROR: Critical CVEs found!"
   exit 1
 fi
