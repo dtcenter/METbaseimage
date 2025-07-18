5e08287114a51e05608000fdce7e2d9b51994b30
John Halley Gotway <johnhg@ucar.edu>
2025-07-18 12:11:18 -0600

    Port over recent enhancements from the feature_internal_61_main_v3.3_cves branch.

--- .github/jobs/scan_docker_image.sh
+++ .github/jobs/scan_docker_image.sh
@@ -3,7 +3,12 @@
 source ${GITHUB_WORKSPACE}/.github/jobs/bash_functions.sh
 
 # Scan the images
-cve_scan_image ${DOCKERHUB_BASE_REPO}:${GITHUB_NAME}
-cve_scan_image ${DOCKERHUB_UNIT_TEST_REPO}:${GITHUB_NAME}
-cve_scan_image ${DOCKERHUB_METVIEWER_REPO}:${GITHUB_NAME}
+ret1 = cve_scan_image ${DOCKERHUB_BASE_REPO}:${GITHUB_NAME}
+ret2 = cve_scan_image ${DOCKERHUB_UNIT_TEST_REPO}:${GITHUB_NAME}
+ret3 = cve_scan_image ${DOCKERHUB_METVIEWER_REPO}:${GITHUB_NAME}
 
+# Check for bad return status
+if [[ $ret1 -ne 0 || $ret2 -ne 0 || $ret3 -ne0 ]]; then
+  echo "ERROR: Critical CVEs found!"
+  exit 1
+fi
