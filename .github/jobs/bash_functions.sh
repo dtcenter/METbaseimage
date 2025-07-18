e87b8c2baa9c66f305b87d5d9a6d724a6e9ad918
John Halley Gotway <johnhg@ucar.edu>
2025-07-18 11:50:54 -0600

    Explicity return 0 from scan_single_docker_image.sh to hopefully prevent the job from failing.

--- .github/jobs/bash_functions.sh
+++ .github/jobs/bash_functions.sh
@@ -42,7 +42,7 @@ function cve_scan_image {
   # return bad status for non-zero Criticals
   N_CRITICAL=`grep " Critical " ${CMD_LOGFILE} | wc -l`
   if [ ${N_CRITICAL} -gt 0 ]; then
-    echo "WARNING: Found ${N_CRITICAL} CVEs for image $1 in ${CMD_LOGFILE}"
+    echo "WARNING: Found ${N_CRITICAL} Critical CVEs for image $1 in ${CMD_LOGFILE}"
     echo
     egrep "SEVERITY|Critical" ${CMD_LOGFILE}
     echo
