05a9fe1583711833d72fa6371dbbc94b0fbae33c
John Halley Gotway <johnhg@ucar.edu>
2025-07-25 12:05:02 -0600

    Per #37, improve error handling in the job scripts and add update_latest option to the release workflow.

--- .github/jobs/build_docker_image.sh
+++ .github/jobs/build_docker_image.sh
@@ -12,42 +12,38 @@ source ${GITHUB_WORKSPACE}/.github/jobs/bash_functions.sh
 MET_BASE_TAG=${GITHUB_NAME}
 
 # Build dtcenter/met-base
-DOCKERHUB_TAG_BASE=${DOCKERHUB_BASE_REPO}:${GITHUB_NAME}
+DOCKERHUB_TAG_BASE=$(get_dockerhub_tag ${DOCKERHUB_BASE_REPO} ${GITHUB_NAME})
 DOCKERFILE_PATH=${GITHUB_WORKSPACE}/${GITHUB_NAME}/Dockerfile
 CMD_LOGFILE=${GITHUB_WORKSPACE}/docker_build_met_base_image.log
 
-time_command docker build -t ${DOCKERHUB_TAG_BASE} \
-    -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}
-if [ $? != 0 ]; then
+if ! $(time_command docker build -t ${DOCKERHUB_TAG_BASE} \
+       -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}); then
   cat ${GITHUB_WORKSPACE}/docker_build_met_base_image.log
   exit 1
 fi
 
 # Build dtcenter/met-base-unit-test
-DOCKERHUB_TAG_UNIT_TEST=${DOCKERHUB_UNIT_TEST_REPO}:${GITHUB_NAME}
+DOCKERHUB_TAG_UNIT_TEST=$(get_dockerhub_tag ${DOCKERHUB_UNIT_TEST_REPO} ${GITHUB_NAME})
 DOCKERFILE_PATH=${GITHUB_WORKSPACE}/${GITHUB_NAME}/Dockerfile.unit_test_env
 CMD_LOGFILE=${GITHUB_WORKSPACE}/docker_build_met_base_unit_test_env_image.log
 
-time_command docker build -t ${DOCKERHUB_TAG_UNIT_TEST} \
-    --build-arg MET_BASE_REPO=${DOCKERHUB_BASE_REPO} \
-    --build-arg MET_BASE_TAG=${MET_BASE_TAG} \
-    -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}
-if [ $? != 0 ]; then
+if ! $(time_command docker build -t ${DOCKERHUB_TAG_UNIT_TEST} \
+       --build-arg MET_BASE_REPO=${DOCKERHUB_BASE_REPO} \
+       --build-arg MET_BASE_TAG=${MET_BASE_TAG} \
+       -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}); then
   cat ${GITHUB_WORKSPACE}/docker_build_met_base_unit_test_env_image.log
   exit 1
 fi
 
 # Build dtcenter/met-base-metviewer
-DOCKERHUB_TAG_METVIEWER=${DOCKERHUB_METVIEWER_REPO}:${GITHUB_NAME}
+DOCKERHUB_TAG_METVIEWER=$(get_dockerhub_tag ${DOCKERHUB_METVIEWER_REPO} ${GITHUB_NAME})
 DOCKERFILE_PATH=${GITHUB_WORKSPACE}/${GITHUB_NAME}/Dockerfile.metviewer
 CMD_LOGFILE=${GITHUB_WORKSPACE}/docker_build_met_base_metviewer_image.log
 
-time_command docker build -t ${DOCKERHUB_TAG_METVIEWER} \
-    --build-arg MET_BASE_REPO=${DOCKERHUB_BASE_REPO} \
-    --build-arg MET_BASE_TAG=${MET_BASE_TAG} \
-    -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}
-if [ $? != 0 ]; then
+if ! $(time_command docker build -t ${DOCKERHUB_TAG_METVIEWER} \
+       --build-arg MET_BASE_REPO=${DOCKERHUB_BASE_REPO} \
+       --build-arg MET_BASE_TAG=${MET_BASE_TAG} \
+       -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}); then
   cat ${GITHUB_WORKSPACE}/docker_build_met_base_metviewer_image.log
   exit 1
 fi
-
--- .github/jobs/push_docker_image.sh
+++ .github/jobs/push_docker_image.sh
@@ -2,11 +2,11 @@
 
 source ${GITHUB_WORKSPACE}/.github/jobs/bash_functions.sh
 
-DOCKERHUB_TAG_BASE=${DOCKERHUB_BASE_REPO}:${GITHUB_NAME}
-DOCKERHUB_TAG_UNIT_TEST=${DOCKERHUB_UNIT_TEST_REPO}:${GITHUB_NAME}
-DOCKERHUB_TAG_METVIEWER=${DOCKERHUB_METVIEWER_REPO}:${GITHUB_NAME}
+DOCKERHUB_TAG_BASE=$(get_dockerhub_tag ${DOCKERHUB_BASE_REPO} ${GITHUB_NAME})
+DOCKERHUB_TAG_UNIT_TEST=$(get_dockerhub_tag ${DOCKERHUB_UNIT_TEST_REPO} ${GITHUB_NAME})
+DOCKERHUB_TAG_METVIEWER=$(get_dockerhub_tag ${DOCKERHUB_METVIEWER_REPO} ${GITHUB_NAME})
 
-# skip docker push if credentials are not set
+# Skip docker push if credentials are not set
 if [ -z ${DOCKER_USERNAME+x} ] || [ -z ${DOCKER_PASSWORD+x} ]; then
   echo "DockerHub credentials not set. Skipping docker push"
   exit 1
@@ -15,20 +15,24 @@ fi
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
 # Push dtcenter/met-base
-time_command docker push ${DOCKERHUB_TAG_BASE}
-if [ $? != 0 ]; then
-  exit 1
-fi
+time_command_exit docker push ${DOCKERHUB_TAG_BASE}
 
 # Push dtcenter/met-base-unit-test
-time_command docker push ${DOCKERHUB_TAG_UNIT_TEST}
-if [ $? != 0 ]; then
-  exit 1
-fi
+time_command_exit docker push ${DOCKERHUB_TAG_UNIT_TEST}
 
 # Push dtcenter/met-base-metviewer
-time_command docker push ${DOCKERHUB_TAG_METVIEWER}
-if [ $? != 0 ]; then
-  exit 1
-fi
+time_command_exit docker push ${DOCKERHUB_TAG_METVIEWER}
 
+# For the release-docker-images.yml workflow, push X.Y-latest for vX.Y.Z versions
+if [[ "${UPDATE_LATEST}" == "true" && "${GITHUB_NAME}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  LATEST_TAG=$(echo ${GITHUB_NAME} | sed 's/^v//g' | cut -f1,2 -d'.')-latest
+
+  time_command_exit docker tag ${DOCKERHUB_TAG_BASE} ${DOCKERHUB_BASE_REPO}:${LATEST_TAG}
+  time_command_exit docker push ${DOCKERHUB_BASE_REPO}:${LATEST_TAG}
+
+  time_command_exit docker tag ${DOCKERHUB_TAG_UNIT_TEST} ${DOCKERHUB_UNIT_TEST_REPO}:${LATEST_TAG}
+  time_command_exit docker push ${DOCKERHUB_UNIT_TEST_REPO}:${LATEST_TAG}
+
+  time_command_exit docker tag ${DOCKERHUB_TAG_METVIEWER} ${DOCKERHUB_METVIEWER_REPO}:${LATEST_TAG}
+  time_command_exit docker push ${DOCKERHUB_METVIEWER_REPO}:${LATEST_TAG}
+fi
