8f8fe957c737597946ac5e805f8bb763d75d836a
John Halley Gotway <johnhg@ucar.edu>
2025-07-26 20:47:55 -0600

    Feature #37 develop push_latest (#38)
    * Per #37, improve error handling in the job scripts and add update_latest option to the release workflow.
    
    * Per #37, add the continue-on-error option when scanning images for CVEs. When critical CVEs are found, the scan step should fail, but we should proceed with pushing the result to Docker Hub for further inspection and debugging.
    
    * Per #37, revise logic to continue-on-error for dev images but not releases.
    
    * Per #37, refine syntax in the build_docker_image.sh script.
    
    * Per #37, use ::group:: and ::endgroup:: to make the docker logs collapsible via GitHub actions.
    
    * Per #37, use ::group:: and ::endgroup:: to make the docker logs collapsible via GitHub actions.

--- .github/jobs/bash_functions.sh
+++ .github/jobs/bash_functions.sh
@@ -7,25 +7,42 @@ function time_command {
   local start_seconds=$SECONDS
   echo "::group::RUNNING: $*"
 
-  local error
+  local retval
   # pipe output to log file if set
   if [ "x$CMD_LOGFILE" == "x" ]; then
-      "$@"
-      error=$?
+    "$@"
+    retval=$?
   else
-      echo "Logging to ${CMD_LOGFILE}"
-      "$@" >> $CMD_LOGFILE 2>&1
-      error=$?
-      unset CMD_LOGFILE
+    echo "Logging to ${CMD_LOGFILE}"
+    "$@" >> $CMD_LOGFILE 2>&1
+    retval=$?
+    unset CMD_LOGFILE
   fi
 
   local duration=$(( SECONDS - start_seconds ))
   echo "TIMING: Command took `printf '%02d' $(($duration / 60))`:`printf '%02d' $(($duration % 60))` (MM:SS): '$*'"
   echo "::endgroup::"
-  if [ ${error} -ne 0 ]; then
-    echo "ERROR: '$*' exited with status = ${error}"
+  if [ $retval -ne 0 ]; then
+    echo "ERROR: '$*' exited with status = ${retval}"
   fi
-  return $error
+  return $retval
+}
+
+# run command and exit on bad status
+function time_command_exit {
+  retval=$(time_command $@)
+  if [ $retval -ne 0 ]; then
+    exit $retval
+  fi
+  return $retval
+}
+
+# utility function to construct the DockerHub tag name to be used,
+# replacing slashes with underscores in the branch name, and
+# omitting the leading 'v' from the DockerHub version
+
+function get_dockerhub_tag {
+  echo ${1}:$(echo ${2} | sed 's%/%_%g' | sed 's%^v%%g' )
 }
 
 # utilty function to scan a Docker image for vulnerabilities
