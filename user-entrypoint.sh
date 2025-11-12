c8fc2116c224892459a35053d96b7dbeaca64cc0
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2025-11-12 10:30:40 -0700

    if uid and gid passed to container are root, then run as root. If they are passed but not root, change the metplus_user uid/gid and run as that user. Otherwise, run as the metplus_user without changing it

--- user-entrypoint.sh
+++ user-entrypoint.sh
@@ -2,19 +2,27 @@
 
 # If USER_ID and GROUP_ID are provided, modify the metplus_user accordingly
 if [ -n "${USER_ID:-}" ] && [ -n "${GROUP_ID:-}" ]; then
-    echo "Adjusting user metplus_user to UID:GID = $USER_ID:$GROUP_ID"
+      # Special case: if both USER_ID and GROUP_ID are 0, run as root
+    if [ "$USER_ID" = "0" ] && [ "$GROUP_ID" = "0" ]; then
+        echo "Running as root (UID:GID = 0:0)"
+        exec "$@"
+    else
+        echo "Adjusting user metplus_user to UID:GID = $USER_ID:$GROUP_ID"
 
-    # Modify group first
-    groupmod -g "$GROUP_ID" metplus_user 2>/dev/null || \
-        groupadd -g "$GROUP_ID" metplus_user 2>/dev/null
+        # Modify group first
+        groupmod -g "$GROUP_ID" metplus_user 2>/dev/null || \
+            groupadd -g "$GROUP_ID" metplus_user 2>/dev/null
 
-    # Modify user
-    usermod -u "$USER_ID" -g "$GROUP_ID" metplus_user 2>/dev/null
+        # Modify user
+        usermod -u "$USER_ID" -g "$GROUP_ID" metplus_user 2>/dev/null
 
-    # Fix ownership of directories that metplus_user needs to write to
-    chown -R metplus_user:metplus_user /home/metplus_user 2>/dev/null
+        # Fix ownership of directories that metplus_user needs to write to
+        chown -R metplus_user:metplus_user /home/metplus_user 2>/dev/null
 
+        # Switch to metplus_user and execute the command
+        exec gosu metplus_user "$@"
+    fi
+else
+    # No USER_ID/GROUP_ID provided, use default metplus_user
+    exec gosu metplus_user "$@"
 fi
-
-# Switch to metplus_user and execute the command
-exec gosu metplus_user "$@"
