7353480835b5f34ea59da26fd392f615084fee9e
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2026-01-15 15:42:55 -0700

    Feature #52 main_v3.4 - Separate MET and METviewer images (#53)
    * Split up METviewer and MET (with and without unit test reqs) to run in parallel since METviewer will no longer be based on MET base image
    
    * Per #52, create METviewer base image from Ironbank debian:12-slim image and install any requirements that are needed from the MET base image Dockerfile
    
    * allow override of base repo to allow IronBank debian image to be used for MET base image if desired in the future
    
    * add useful defaults for base image
    
    * install git because it is needed to obtain METviewer source code
    
    * fix missing and
    
    * exit success after met builds
    
    * remove libaom3 that has critical CVE
    
    * various changes applied to main_v3.5 branch -- remove MET_ from BASE_REPO/TAG, change scan log artifact name to prevent conflict, do not fail on critical CVEs, rename build log artifact in dev workflow to be consistent with release workflow. Also, upload build log artifacts before scan so that they are made available even if the scan fails
    
    * replace BASE/DEBIAN_REPO with BASE/DEBIAN_REGISTRY/IMAGE to align with IronBank Docker build arg syntax
    
    * fix identation
    
    * fix incorrect if/and statement
    
    * remove python3.11 which is installed with chrome and causing critical CVEs to be flagged
    
    * add ghostscript because it is needed by R in METviewer
    
    * taking out removal of libxml2 because it causes chrome to be uninstalled as well

--- .github/jobs/build_docker_image.sh
+++ .github/jobs/build_docker_image.sh
@@ -8,36 +8,51 @@ source ${GITHUB_WORKSPACE}/.github/jobs/bash_functions.sh
 #   $DOCKERHUB_BASE_REPO is dtcenter/met-base(-dev).
 #   $DOCKERHUB_UNIT_TEST_REPO is dtcenter/met-base-unit-test(-dev).
 #   $DOCKERHUB_METVIEWER_REPO is dtcenter/met-base-metviewer(-dev).
+#   $DEBIAN_REGISTRY is debian Docker registry name -- IronBank or docker.io
+#   $DEBIAN_IMAGE is debian Docker image name -- IronBank or DockerHub
+#   $METPLUS_COMPONENT is component to build -- either met or metviewer
 
-# remove leading 'v' from version tag
-MET_BASE_TAG=$(echo ${GITHUB_NAME} | sed 's%/%_%g' | sed 's%^v%%g' )
+if [ "$METPLUS_COMPONENT" == "met" ]; then
+    # remove leading 'v' from version tag
+    MET_BASE_TAG=$(echo ${GITHUB_NAME} | sed 's%/%_%g' | sed 's%^v%%g' )
 
-# Build dtcenter/met-base
-DOCKERHUB_TAG_BASE=$(get_dockerhub_tag ${DOCKERHUB_BASE_REPO} ${GITHUB_NAME})
-DOCKERFILE_PATH=${GITHUB_WORKSPACE}/${GITHUB_NAME}/Dockerfile
-CMD_LOGFILE=${GITHUB_WORKSPACE}/docker_build_met_base_image.log
+    # Build dtcenter/met-base
+    DOCKERHUB_TAG_BASE=$(get_dockerhub_tag ${DOCKERHUB_BASE_REPO} ${GITHUB_NAME})
+    DOCKERFILE_PATH=${GITHUB_WORKSPACE}/${GITHUB_NAME}/Dockerfile
+    CMD_LOGFILE=${GITHUB_WORKSPACE}/docker_build_met_base_image.log
 
-if ! time_command docker build -t ${DOCKERHUB_TAG_BASE} \
-     -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}; then
-  echo "::group::${GITHUB_WORKSPACE}/docker_build_met_base_image.log"
-  cat ${GITHUB_WORKSPACE}/docker_build_met_base_image.log
-  echo "::endgroup::"
-  exit 1
+    if ! time_command docker build -t ${DOCKERHUB_TAG_BASE} \
+             --build-arg BASE_REGISTRY=${DEBIAN_REGISTRY} \
+             --build-arg BASE_IMAGE=${DEBIAN_IMAGE} \
+             -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}; then
+      echo "::group::${GITHUB_WORKSPACE}/docker_build_met_base_image.log"
+      cat ${GITHUB_WORKSPACE}/docker_build_met_base_image.log
+      echo "::endgroup::"
+      exit 1
+    fi
+
+    # Build dtcenter/met-base-unit-test
+    DOCKERHUB_TAG_UNIT_TEST=$(get_dockerhub_tag ${DOCKERHUB_UNIT_TEST_REPO} ${GITHUB_NAME})
+    DOCKERFILE_PATH=${GITHUB_WORKSPACE}/${GITHUB_NAME}/Dockerfile.unit_test_env
+    CMD_LOGFILE=${GITHUB_WORKSPACE}/docker_build_met_base_unit_test_env_image.log
+
+    if ! time_command docker build -t ${DOCKERHUB_TAG_UNIT_TEST} \
+             --build-arg MET_BASE_REPO=${DOCKERHUB_BASE_REPO} \
+             --build-arg MET_BASE_TAG=${MET_BASE_TAG} \
+             -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}; then
+      echo "::group::${GITHUB_WORKSPACE}/docker_build_met_unit_test_env_image.log"
+      cat ${GITHUB_WORKSPACE}/docker_build_met_base_unit_test_env_image.log
+      echo "::endgroup::"
+      exit 1
+    fi
+    # end of MET and MET unit test section
+    exit 0
 fi
 
-# Build dtcenter/met-base-unit-test
-DOCKERHUB_TAG_UNIT_TEST=$(get_dockerhub_tag ${DOCKERHUB_UNIT_TEST_REPO} ${GITHUB_NAME})
-DOCKERFILE_PATH=${GITHUB_WORKSPACE}/${GITHUB_NAME}/Dockerfile.unit_test_env
-CMD_LOGFILE=${GITHUB_WORKSPACE}/docker_build_met_base_unit_test_env_image.log
-
-if ! time_command docker build -t ${DOCKERHUB_TAG_UNIT_TEST} \
-     --build-arg MET_BASE_REPO=${DOCKERHUB_BASE_REPO} \
-     --build-arg MET_BASE_TAG=${MET_BASE_TAG} \
-     -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}; then
-  echo "::group::${GITHUB_WORKSPACE}/docker_build_met_unit_test_env_image.log"
-  cat ${GITHUB_WORKSPACE}/docker_build_met_base_unit_test_env_image.log
-  echo "::endgroup::"
-  exit 1
+# exit if METplus component is not set to either met or metviewer
+if [ "$METPLUS_COMPONENT" != "metviewer" ]; then
+    echo "ERROR: METPLUS_COMPONENT must be set to either met or metviewer: ${METPLUS_COMPONENT}"
+    exit 1
 fi
 
 # Build dtcenter/met-base-metviewer
@@ -46,9 +61,9 @@ DOCKERFILE_PATH=${GITHUB_WORKSPACE}/${GITHUB_NAME}/Dockerfile.metviewer
 CMD_LOGFILE=${GITHUB_WORKSPACE}/docker_build_met_base_metviewer_image.log
 
 if ! time_command docker build -t ${DOCKERHUB_TAG_METVIEWER} \
-     --build-arg MET_BASE_REPO=${DOCKERHUB_BASE_REPO} \
-     --build-arg MET_BASE_TAG=${MET_BASE_TAG} \
-     -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}; then
+         --build-arg BASE_REGISTRY=${DEBIAN_REGISTRY} \
+         --build-arg BASE_IMAGE=${DEBIAN_IMAGE} \
+         -f $DOCKERFILE_PATH ${GITHUB_WORKSPACE}; then
   echo "::group::${GITHUB_WORKSPACE}/docker_build_met_base_metviewer_image.log"
   cat ${GITHUB_WORKSPACE}/docker_build_met_base_metviewer_image.log
   echo "::endgroup::"
